1c3f5164991879a39245beb9db1d2933697a69e7
Thomas Oster <thomas.oster@rwth-aachen.de>
2013-01-21 19:50:17 +0100

    simple packing script

---
+++ createZip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Creates a Zip file ready to publish
+version=$(git describe --tags --dirty --always)
+rm -rf dist/*
+ant jar
+cp -r html dist/
+cd dist
+zip -r ../VisiCam-$version.zip .
+cd ..
+cp -r lib/javacv-pi/* dist/lib/
+cd dist
+zip -r ../VisiCam-PI-$version.zip .
