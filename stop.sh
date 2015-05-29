2dfd28ae2e663156dc37d5478f1d500cbc983edd
FroChr123 <frochr123@trash-mail.com>
2015-05-29 04:52:43 +0200

    Bash scripts: Add or change bash scripts for easier handling of VisiCam

---
+++ stop.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#    This file is part of VisiCam. (https://github.com/t-oster/VisiCam)
+#
+#    VisiCam is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    VisiCam is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with VisiCam.  If not, see <http://www.gnu.org/licenses/>.
+
+# Stop VisiCam
+pkill -9 -f VisiCam.jar
